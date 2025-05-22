f0b00258410edc2bbadb0062d00d084b8d6d5154
Sir-Leon <eb3.61594.22@student.chuka.ac.ke>
2025-05-22 10:47:56 +0300

    Arrays and loops

---
+++ javascript-learning/00-basics/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+// Copies 1-multi_languages.js but by using an array of string and a loop
+
+const coding = ["C is fun", "Python is cool", "Javascript is amazing"];
+for (let i = 0; i < coding.length; i++) {
+    console.log(coding[i]);
+}
